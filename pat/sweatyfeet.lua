00dc181861da91a71d079b93b02b8613e3f30fb0
patman <27874300+patmanf@users.noreply.github.com>
2021-11-06 01:54:00 -0500

    fixed bug that made the water spawn only every 2 seconds by using code

--- pat/sweatyfeet.lua
+++ pat/sweatyfeet.lua
@@ -1,16 +1,17 @@
 --spawn water at the player's feet every second
-local seconds,Liquid,Forever,t={[10]=1},{water=1},"f",0
-local function wait(s)t=t-Dt;if t<=0 then t=s;return true end end
-local function spawn(l)return l end;local Player=spawn
+local Liquid,Forever,t={water=1},"f",0
+local function seconds(s)t=t-Dt;if t<=0 then t=s/10;return true end end
+local Do={spawn=function(_,l) return l end}
+local function Player(f) return f end
 local Playelocation={feet=function()local p=mcontroller.position();return{p[1],p[2]+mcontroller.boundBox()[2]}end}
-local Repeat={f=function(a)if wait(a.wait)then world.spawnLiquid(a.on(),a.Do,1)end end}
+local Repeat={f=function(a)if a.wait then world.spawnLiquid(a.on(),a[1],1)end end}
 function update(dt)Dt=dt
 
 
 --or, in coding terms
  Repeat[Forever] {
-wait = seconds[10],
-Do = spawn(Liquid.water), on = Player(Playelocation.feet) };
+wait=seconds(10),
+Do:spawn(Liquid.water), on=Player(Playelocation.feet) };
 
 
 end
